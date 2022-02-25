c82b6b285fad80c38474967c72d1b529722119a6
Cortney Weintz <cweintz@stanford.edu>
2022-02-24 17:33:18 -0800

    added user reporting skeleton

--- DiscordBot/report.py
+++ DiscordBot/report.py
@@ -4,14 +4,33 @@ import re
 
 class State(Enum):
     REPORT_START = auto()
+
     AWAITING_MESSAGE = auto()
-    MESSAGE_IDENTIFIED = auto()
+
+    AWAITING_REASON = auto()
+
+    AWAITING_THREAT = auto()
+    AWAITING_SUICIDAL = auto()
+
+    AWAITING_HARASSMENT = auto()
+
+    AWAITING_SPAM = auto()
+
+    AWAITING_SUSPICIOUS = auto()
+    AWAITING_AUTHORITIES = auto()
+
+    AWAITING_BLOCK = auto()
+
+    AWAITING_INFO = auto()
+
     REPORT_COMPLETE = auto()
 
 class Report:
     START_KEYWORD = "report"
     CANCEL_KEYWORD = "cancel"
     HELP_KEYWORD = "help"
+    BLOCK_REQUEST = "Would you like to block this user from sending you more messages? (yes/no)"
+    INFO_REQUEST = "If you would like to provide additional information, please reply to this message with the additional information that you would like to provide (all in one message). Otherwise, reply 'no'."
 
     def __init__(self, client):
         self.state = State.REPORT_START
@@ -31,7 +50,7 @@ class Report:
             return ["Report cancelled."]
         
         if self.state == State.REPORT_START:
-            reply =  "Thank you for starting the reporting process. "
+            reply = "Thank you for starting the reporting process. "
             reply += "Say `help` at any time for more information.\n\n"
             reply += "Please copy paste the link to the message you want to report.\n"
             reply += "You can obtain this link by right-clicking the message and clicking `Copy Message Link`."
@@ -54,18 +73,161 @@ class Report:
             except discord.errors.NotFound:
                 return ["It seems this message was deleted or never existed. Please try again or say `cancel` to cancel."]
 
-            # Here we've found the message - it's up to you to decide what to do next!
-            self.state = State.MESSAGE_IDENTIFIED
-            return ["I found this message:", "```" + message.author.name + ": " + message.content + "```", \
-                    "This is all I know how to do right now - it's up to you to build out the rest of my reporting flow!"]
-        
-        if self.state == State.MESSAGE_IDENTIFIED:
-            return ["<insert rest of reporting flow here>"]
+            # Here we've found the message
+            reply = "I found this message:\n" + "```" + message.author.name + ": " + message.content + "``` \n"
+            reply += "What is your reason for reporting this message? "
+            reply += "Reply '1' for Threat of Danger/Harm, "
+            reply += "'2' for Harassment, "
+            reply += "'3' for Spam, or "
+            reply += "'4' for Suspicious behavior."
+            self.state = State.AWAITING_REASON
+            return [reply]
+
+        if self.state == State.AWAITING_REASON:
+            m = message.content
+            reply = "I don't understand that response. Please reply with either '1', '2', '3', or '4'."
+            if m == "1":
+                reply = "You selected 'Threat of Danger/Harm'. "
+                reply += "How would you categorize the threat of danger/harm? "
+                reply += "Reply '1' for Credible Threat of Violence or '2' for Suicidal Comments."
+                self.state = State.AWAITING_THREAT
+            elif m == "2":
+                reply = "You selected 'Harassment'. "
+                reply += "How would you categorize the harassment? "
+                reply += "Reply '1' for Sexual Harassment, '2' for Hate Speech, or '3' for Bullying."
+                self.state = State.AWAITING_HARASSMENT
+            elif m == "3":
+                reply = "You selected 'Spam'. "
+                reply += "How would you categorize the spam? "
+                reply += "Reply '1' for Unwanted Solicitation or '2' for Scam/Fraudulent Business."
+                self.state = State.AWAITING_SPAM
+            elif m == "4":
+                reply = "You selected 'Suspicious Behavior'. "
+                reply += "How would you categorize the suspicious behavior? "
+                reply += "Reply '1' for Possible Grooming, '2' for Impersonation/Compromised Account, '3' for Attempting to Solicit Personal Information, or '4' for Offer of Transportation."
+                self.state = State.AWAITING_SUSPICIOUS
+            return [reply]
+
+        if self.state == State.AWAITING_THREAT:
+            m = message.content
+            reply = "I don't understand that response. Please reply with either '1' or '2'."
+            if m == "1":
+                reply = "You selected 'Credible Threat of Violence'. "
+                reply += "If you feel that the threat is imminent, consider calling 911."
+                reply += self.BLOCK_REQUEST
+                self.state = State.AWAITING_BLOCK
+            elif m == "2":
+                reply = "You selected 'Suicidal Comments'. "
+                reply += "Would you would like us to send this account an anonymous message with suicide prevention resources? (yes/no)"
+                self.state = State.AWAITING_SUICIDAL
+            return [reply]
+
+        if self.state == State.AWAITING_HARASSMENT:
+            m = message.content
+            reply = "I don't understand that response. Please reply with either '1', '2', or '3'."
+            if m == "1":
+                reply = "You selected 'Sexual Harassment'. "
+                reply += "We will investigate this report and potentially ban this user. "
+                reply += self.BLOCK_REQUEST
+                self.state = State.AWAITING_BLOCK
+            elif m == "2":
+                reply = "You selected 'Hate Speech'. "
+                reply += "We will investigate this report and potentially ban this user. "
+                reply += self.BLOCK_REQUEST
+                self.state = State.AWAITING_BLOCK
+            elif m == "3":
+                reply = "You selected 'Bullying'. "
+                reply += "We will investigate this report and potentially ban this user. "
+                reply += self.BLOCK_REQUEST
+                self.state = State.AWAITING_BLOCK
+            return [reply]
+
+        if self.state == State.AWAITING_SPAM:
+            m = message.content
+            reply = "I don't understand that response. Please reply with either '1' or '2'."
+            if m == "1":
+                reply = "You selected 'Unwanted Solicitation'. "
+                reply += "We will investigate this report and potentially ban this user. "
+                reply += self.BLOCK_REQUEST
+                self.state = State.AWAITING_BLOCK
+            elif m == "2":
+                reply = "You selected 'Scam/Fraudulent Business'. "
+                reply += "We will investigate this report and potentially ban this user. "
+                reply += self.BLOCK_REQUEST
+                self.state = State.AWAITING_BLOCK
+            return [reply]
+
+        if self.state == State.AWAITING_SUSPICIOUS:
+            m = message.content
+            reply = "I don't understand that response. Please reply with either '1', '2', '3', or '4'."
+            if m == "1":
+                reply = "You selected 'Possible Grooming'. "
+                reply += "Is this a serious matter that may potentially involve the authorities? (yes/no)"
+                self.state = State.AWAITING_AUTHORITIES
+            elif m == "2":
+                reply = "You selected 'Impersonation/Compromised Account'. "
+                reply += "We will investigate the suspicious behavior and take action if necessary. "
+                reply += self.BLOCK_REQUEST
+                self.state = State.AWAITING_BLOCK
+            elif m == "3":
+                reply = "You selected 'Attempting to Solicit Personal Information'. "
+                reply += "Is this a serious matter that may potentially involve the authorities? (yes/no)"
+                self.state = State.AWAITING_AUTHORITIES
+            elif m == "4":
+                reply = "You selected 'Offer of Transportation'. "
+                reply += "Is this a serious matter that may potentially involve the authorities? (yes/no)"
+                self.state = State.AWAITING_AUTHORITIES
+            return [reply]
+
+        if self.state == State.AWAITING_AUTHORITIES:
+            m = message.content
+            reply = "I don't understand that response. Please reply with either 'yes' or 'no'."
+            if m == "yes":
+                reply = "A member of our team will investigate the suspicious behavior and alert authorities if necessary. "
+                reply += self.BLOCK_REQUEST
+                self.state = State.AWAITING_BLOCK
+            elif m == "no":
+                reply = "We will investigate the suspicious behavior and take action if necessary."
+                reply += self.BLOCK_REQUEST
+                self.state = State.AWAITING_BLOCK
+            return [reply]
+
+        if self.state == State.AWAITING_SUICIDAL:
+            m = message.content
+            reply = "I don't understand that response. Please reply with either 'yes' or 'no'."
+            if m == "yes":
+                reply = "The message has been sent, and resources have been shared anonymously with the user in concern. "
+                reply += self.BLOCK_REQUEST
+                self.state = State.AWAITING_BLOCK
+            elif m == "no":
+                reply = self.BLOCK_REQUEST
+                self.state = State.AWAITING_BLOCK
+            return [reply]
+
+        if self.state == State.AWAITING_BLOCK:
+            m = message.content
+            reply = "I don't understand that response. Please reply with either 'yes' or 'no'."
+            if m == "yes":
+                reply = "The user has been blocked. "
+                reply += self.INFO_REQUEST
+                self.state = State.AWAITING_INFO
+            elif m == "no":
+                reply = self.INFO_REQUEST
+                self.state = State.AWAITING_INFO
+            return [reply]
+
+        if self.state == State.AWAITING_INFO:
+            m = message.content
+            reply = "Thank you for your report."
+            if m != "no":
+                reply = "We've attached the additional information you've provided to your report. " + reply
+            self.state = State.REPORT_COMPLETE
+            return [reply]
 
         return []
 
-    def report_complete(self):
-        return self.state == State.REPORT_COMPLETE
+    # def report_complete(self):
+    #     return self.state == State.REPORT_COMPLETE
     
 
 
